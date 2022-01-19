78b0e60683c61681a6b594f6256a1d5cc5f0362e
Lance Kelley <lancekkcodes@gmail.com>
2022-01-19 08:20:45 -0600

    redux store, reducers, and provider

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,9 @@ import Nav from './components/Nav';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
+import {Provider} from 'react-redux';
+import store from './utils/store';
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -42,16 +45,18 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/success" component={Success} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
+          <Provider store={store}>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import { combineReducers } from "redux";
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,7 +12,15 @@ import {
   TOGGLE_CART
 } from "./actions";
 
-export const reducer = (state, action) => {
+const initialState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS:
       return {
@@ -88,3 +97,7 @@ export const reducer = (state, action) => {
 export function useProductReducer(initialState) {
   return useReducer(reducer, initialState)
 }
+
+export default combineReducers({
+  shop: reducer
+});
